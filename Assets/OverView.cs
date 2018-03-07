59305699ab1d34a73c0a8ab416de946fa42bce4d
abarabone <abara00@live.jp>
2018-03-07 19:41:08 +0900

    add CustomIK, OverView

---
+++ Assets/OverView.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PracticeUnity
+{
+	
+	public interface IPoolableObject
+	{
+		void PassPool();
+		void Back();
+	}
+	public interface IObjectPool
+	{
+		IPoolableObject Rent();
+	}
+
+	public class GameObjectPool : IObjectPool
+	{
+		private List<IPoolableObject>	objects;
+
+		public IPoolableObject Rent()
+		{
+			return null;
+		}
+	}
+	
+
+	public interface ICharacterObject
+	{
+		
+	}
+	public class CharacterObject
+	{
+		
+	}
+	public interface ICharacterActionModel
+	{
+		
+	}
+	public class PlayerLocalActionModel : ICharacterActionModel
+	{ 
+		
+	}
+	public class PlayerRemoteActionModel : ICharacterActionModel
+	{ 
+		
+	}
+
+	public interface IStructureObject
+	{
+
+	}
+	public interface IPartObject
+	{ 
+		
+	}
+
+
+	public class Building : IStructureObject
+	{ 
+		
+	}
+	public class PlotField : IStructureObject
+	{
+
+	}
+	public class RoadField : IStructureObject
+	{
+
+	}
+	public interface IHitTarget
+	{
+		
+	}
+
+	
+	public class WideArea : IStructureObject
+	{ 
+		
+	}
+	public class StructureBinder : IStructureObject
+	{ 
+		
+	}
+	
+
+	public interface IWapon
+	{ 
+		void Switch();
+		void Select( uint id );
+	}
+	public interface IFireUnit
+	{ 
+		void Fire();
+		void Reload();
+	}
+	public interface ITriggerUnit
+	{ 
+		void Pulse();
+		void Bulk( uint freq );
+		void Single();
+	}
+	
+	public interface IEmitable
+	{
+		
+	}
+	public interface IEmitted
+	{
+
+	}
+}
+
+
+
---
+++ Assets/Sample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PracticeUnity;
+
+public class Sample : MonoBehaviour
+{
+	
+	public ICharacterActionModel	ch;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
