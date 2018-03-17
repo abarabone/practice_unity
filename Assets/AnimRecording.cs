ef8514b358739c584f200b01896a17cbfdbaab8b
abarabone <abara00@live.jp>
2018-03-17 21:20:35 +0900

    animtor recording うまくいかないなぁ

---
+++ Assets/AnimRecording.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class AnimRecording : MonoBehaviour
+{
+
+	private IDisposable	ev; 
+
+	private async void OnEnable()
+	{
+		var anim = GetComponent<Animator>();
+
+		anim.StartRecording( 100 );
+
+		await Observable.EveryGameObjectUpdate().Skip( 3 ).First();
+
+		anim.StopRecording();
+		anim.StartPlayback();
+		anim.playbackTime = 0;
+
+		ev = Observable.EveryGameObjectUpdate().Subscribe( _ => anim.playbackTime = 0 );
+	}
+	private void OnDisable()
+	{
+		GetComponent<Animator>().StopPlayback();
+		ev.Dispose();
+	}
+
+
+}
